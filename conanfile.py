ad5ed5442ad1e61d1f8f6b9ffcaa53c24e784907
cppbitman <chinadaihongjun@gmail.com>
2018-11-08 18:04:41 -0800

    linux build

---
+++ conanfile.py
@@ -0,0 +1,60 @@
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
+from shutil import copyfile
+import os
+
+class A52decConan(ConanFile):
+    name = "a52dec"
+    version = "0.7.4"
+    description = "a52dec is a test program for liba52. It decodes ATSC A/52 streams, "
+    "and also includes a demultiplexer for mpeg-1 and mpeg-2 program streams"
+    url = "https://github.com/conan-multimedia/a52dec"
+    homepage = "http://liba52.sourceforge.net/"
+    license = "GPLv2Plus"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    generators = "cmake"
+
+    source_subfolder = "source_subfolder"
+
+    def source(self):
+        url = 'http://liba52.sourceforge.net/files/{name}-{version}.tar.gz'
+        tools.get(url.format(name =self.name, version =self.version))
+        os.rename( self.name + "-" + self.version, self.source_subfolder)
+
+    def build(self):
+        #vars = {'LIBS' : "-lm"}
+
+        #with tools.environment_append(vars):
+        #    self.run("autoreconf -f -i")
+        #    copyfile("%s/data/autotools/config.guess"%(self.cerbero_root), "%s/autotools/config.guess"%(os.getcwd()))
+        #    copyfile("%s/data/autotools/config.sub"%(self.cerbero_root), "%s/autotools/config.sub"%(os.getcwd()))
+        #    self.run('./configure --prefix %s/build --libdir %s/build/lib --disable-maintainer-mode'
+        #    ' --disable-silent-rules --enable-introspection --with-pic --enable-shared'%(os.getcwd(),os.getcwd()))
+        #    self.run('make -j4')
+        #    self.run('make install')
+        
+        with tools.chdir(self.source_subfolder):
+            with tools.environment_append({'LIBS' : "-lm"}):
+                self.run("autoreconf -f -i")
+
+                _args = ["--prefix=%s/builddir"%(os.getcwd()), "--with-pic", "--disable-silent-rules", "--enable-introspection"]
+                if self.options.shared:
+                    _args.extend(['--enable-shared=yes','--enable-static=no'])
+                else:
+                    _args.extend(['--enable-shared=no','--enable-static=yes'])
+
+                autotools = AutoToolsBuildEnvironment(self)
+                autotools.fpic = True
+                autotools.configure(args=_args)
+                autotools.make(args=["-j4"])
+                autotools.install()
+
+    def package(self):
+        if tools.os_info.is_linux:
+            with tools.chdir(self.source_subfolder):
+                self.copy("*", src="%s/builddir"%(os.getcwd()))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+
